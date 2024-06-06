718798ad666933ab6119d66baa5c9ebe8fba260b
Pedro Macedo <pedrofernando@prof.educacao.sp.gov.br>
2024-06-06 12:27:55 -0300

    remove whitespaces

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ function setup() {
   createCanvas(400,400);
   background(random(0,255),random(0,255),random(0,255));
 }
-
 function draw() {
     if (mouseIsPressed){
       fill(random(0,255),random(0,255),random(0,255));
