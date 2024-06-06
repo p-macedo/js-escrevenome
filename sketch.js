7015e308911019783dfa875a304b7a369f2545fa
Pedro Macedo <pedrofernando@prof.educacao.sp.gov.br>
2024-06-06 12:24:09 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400,400);
+  background(random(0,255),random(0,255),random(0,255));
+}
+
+function draw() {
+    if (mouseIsPressed){
+      fill(random(0,255),random(0,255),random(0,255));
+      rect(mouseX,mouseY,20,20);
+    }
+}
